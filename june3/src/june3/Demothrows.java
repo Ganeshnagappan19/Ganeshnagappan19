9d3faef860bc1efd50a2af5bb40021916b477826
ganesh.nagappan <ganesh.nagappan@DT15023.QICHOAPPL.com>
2022-06-03 17:49:19 +0530

    uu

---
+++ june3/src/june3/Demothrows.java
@@ -0,0 +1,32 @@
+package june3;
+
+public class Demothrows {
+	
+		public static void main(String[] args) {// throws Exception {
+			Race race=new Race();
+			System.out.println("race started.....");
+			//race.driveBike(150);
+			try {
+				race.driveBike(150);
+			}catch(Exception e) {
+				System.out.println(e);
+			}
+			System.out.println("race finished...");
+			
+			
+		}
+	}
+	class Race{
+		public void driveBike(int speed) throws Exception{
+			if(speed>100) {
+				throw new Exception();
+			}
+		}
+	}
+
+
+
+
+
+
+
