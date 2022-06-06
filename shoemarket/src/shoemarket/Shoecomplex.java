73c008ea1f0a8e7fbebe6f31b141b141173524a1
ganesh.nagappan <ganesh.nagappan@DT15023.QICHOAPPL.com>
2022-06-06 14:18:56 +0530

    rr

---
+++ shoemarket/src/shoemarket/Shoecomplex.java
@@ -0,0 +1,37 @@
+package shoemarket;
+
+public class Shoecomplex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name;
+    
+    shoefactory  gg =new shoefactory ();
+    
+ 
+	}
+}
+class shoefactory
+	{
+	public void yy() {
+	shoefactory nn=new shoefactory();
+  System.out.println("hhh");
+	{
+	}
+}
+class shoes{
+	public void bata() {
+	 shoecustomer bb =new shoecustomer();
+	 System.out.println("shoe");
+	}
+	
+}
+class shoecustomer{
+	public void ff() {
+	salesman hh =new salesman();
+	System.out.println("super shoe");
+	}	
+}
+class salesman{
+}
+}
